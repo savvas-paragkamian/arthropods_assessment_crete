024932db9e908e379c12e9c99a892df90484b2bd
savvas-paragkamian <s.paragkamian@gmail.com>
2021-11-19 20:15:07 +0200

    test case for Carabus_banoni

--- scripts/arthropoda_crete_assesment.R
+++ scripts/arthropoda_crete_assesment.R
@@ -13,11 +13,9 @@ library(vegan)
 
 arthropods <- readxl::read_excel("../data/arthropoda_crete_nhmc_for_analysis.xlsx")
 
-
-
 # Spatial data
 locations <-  arthropods %>% dplyr::select(subspeciesname,latD, logD, Order) %>% 
-    filter(latD<36, logD<30) %>%
+    filter(latD<38, logD<30) %>%
     na.omit()
 
 locations_shp <- locations %>% dplyr::select(!Order) %>% relocate(latD,logD,subspeciesname)
@@ -55,12 +53,12 @@ ggsave("map_greece_plot_lines.png", plot = map_greece_plot_lines, device = "png"
 
 ### eoo
 
-EOO <- EOO.computing(locations_shp, country_map=hellenic_borders_shp, export_shp=T)
+EOO <- EOO.computing(locations_shp, country_map=hellenic_borders_shp, export_shp=T,write_shp=T)
 
 
 eoo_results <- read_delim("EOO.results.csv", delim=",", col_names=T)
 
-Acanthopetalum_minotauri <- locations %>% filter(subspeciesname=="Acanthopetalum minotauri")
+Acanthopetalum_minotauri <- locations %>% filter(subspeciesname=="Carabus banoni")
 
 Acanthopetalum_minotauri_plot <- ggplot()+
   geom_polygon(data = hellenic_borders_df,aes(x=long, y=lat,group = group),lwd=0.12,color="black")+
@@ -75,5 +73,33 @@ Acanthopetalum_minotauri_plot <- ggplot()+
   theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
 #geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
     
-ggsave("Acanthopetalum_minotauri.png", plot = Acanthopetalum_minotauri_plot, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
+ggsave("Carabus_banoni.png", plot = Acanthopetalum_minotauri_plot, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
 
+
+### Carabus banoni_EOO_poly
+###
+
+carabus_banoni_shp <- rgdal::readOGR(dsn="shapesIUCN/",layer="Carabus banoni_EOO_poly",verbose=TRUE)
+
+proj4string(carabus_banoni_shp) <- CRS("+proj=longlat +datum=WGS84") 
+
+carabus_banoni <- broom::tidy(carabus_banoni_shp)
+bbox_carabus_banoni <- carabus_banoni_shp@bbox
+bbox_carabus_banoni_lat <- bbox_carabus_banoni
+carabus_banoni_points <- locations %>% filter(subspeciesname=="Carabus banoni")
+
+carabus_banoni_plot <- ggplot()+
+  geom_polygon(data = hellenic_borders_df,aes(x=long, y=lat,group = group),lwd=0.12,color="black")+
+  geom_polygon(data = carabus_banoni,aes(x=long, y=lat,group = group),alpha=0.5,lwd=0.12,color="orange")+
+  geom_point(data = carabus_banoni_points,aes(x=logD, y=latD,color=Order),size = 0.2)+
+  labs(x="Longitude",y="Latitude")+
+#  scale_fill_manual(values = c("chartreuse3","purple","cyan3","chocolate2"),labels = c("Natura2000 v30 SCI", "Natura2000 v30 SPA", "Natura2000 v30 SCISPA","Wildlife Refuge"),name="Protected areas")+
+#  scale_color_manual(name="", values = c("Caves"="red"))+
+  scale_x_continuous(breaks = seq(23,27,0.5),limits = c(23,27))+
+  scale_y_continuous(breaks = seq(34,36,0.5),limits = c(34,37))+
+  coord_map(xlim = c(23,27), ylim = c(34,37))+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
+#geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
+    
+ggsave("Carabus_banoni.png", plot = carabus_banoni_plot, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
