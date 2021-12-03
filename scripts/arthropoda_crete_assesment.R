3cd9ebe614a5153bd6e3cb817582b9da52ac4acb
savvas-paragkamian <s.paragkamian@gmail.com>
2021-12-03 21:47:44 +0200

    AOO and EOO assesment

--- scripts/arthropoda_crete_assesment.R
+++ scripts/arthropoda_crete_assesment.R
@@ -8,20 +8,37 @@ library(rgdal)
 library(ConR)
 library(vegan)
 
-###
-###
-
 #arthropods <- readxl::read_excel("../data/arthropoda_crete_nhmc_for_analysis.xlsx")
-
-#arthropods_kriti <- readxl::read_excel("../data/arthropoda_crete_nhmc_for_analysis.xlsx") %>% filter(grepl("^Kriti",distribution)) %>% dplyr::select(-Ergasia)
-
-arthropods_kriti_sheet <- readxl::read_excel("../data/arthropoda_crete_nhmc_for_analysis.xlsx", sheet="Endemics") %>% dplyr::select(-Ergasia)
+## remove Opiliones because the dataset has some errors that are under review.
+arthropods_kriti <- readxl::read_excel("../data/arthropoda_crete_nhmc_for_analysis.xlsx") %>% 
+    filter(Order!="Opiliones") %>% 
+    dplyr::select(-Ergasia)
 
 # Spatial data
-locations <- arthropods_kriti_sheet %>% dplyr::select(subspeciesname,latD, logD, Order) %>% 
+locations_all <- arthropods_kriti %>% 
+    dplyr::select(subspeciesname,latD, logD, Order) %>% 
     filter(latD<38, logD<30) %>%
     na.omit() %>%
-    mutate(ID=seq(1:nrow(.)))
+    mutate(id=seq(1:nrow(.)))
+
+locations_shp_all <- locations_all %>% 
+    dplyr::select(!Order) %>% 
+    relocate(latD,logD,subspeciesname)
+
+coordinates(locations_shp_all)<-~ logD+latD
+proj4string(locations_shp_all) <- CRS("+proj=longlat +datum=WGS84")
+
+## endemic
+arthropods_kriti_endemic <- readxl::read_excel("../data/arthropoda_crete_nhmc_for_analysis.xlsx", sheet="Endemics") %>% 
+    filter(Order!="Opiliones") %>% 
+    dplyr::select(-Ergasia)
+
+# Spatial data
+locations <- arthropods_kriti_endemic %>% 
+    dplyr::select(subspeciesname,latD, logD, Order) %>% 
+    filter(latD<38, logD<30) %>%
+    na.omit() %>%
+    mutate(id=seq(1:nrow(.)))
 
 locations_shp <- locations %>% dplyr::select(!Order) %>% relocate(latD,logD,subspeciesname)
 coordinates(locations_shp)<-~ logD+latD
@@ -118,30 +135,44 @@ grid_10km <- rgdal::readOGR(dsn="../data/Greece_shapefile/",layer="gr_10km", ver
 # see here https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2 
 grid_10km.wgs <- spTransform(grid_10km, CRS=CRS("+proj=longlat +datum=WGS84"))
 
-
-
-
-over_grid_10k <- sp::over( x = locations_shp , y = grid_10km.wgs , fn = NULL)
-
-species_over_grid <- over_grid_10k %>%
-    mutate(ID=seq(1:nrow(.))) %>% 
-    left_join(locations, by=c("ID"="ID")) %>% 
-    distinct(CELLCODE,subspeciesname) %>%
-    group_by(CELLCODE) %>%
-    summarize(n_species=n())
-
 grid_10km_df <- broom::tidy(grid_10km.wgs)
 
 grid_10km.wgs_data <- grid_10km.wgs@data %>% 
         mutate(id=as.character(seq(0,nrow(.)-1))) %>%
         left_join(grid_10km_df, by=c("id"="id"))
+### all species grid overlap
+
+over_grid_10k_all <- sp::over( x = locations_shp_all , y = grid_10km.wgs , fn = NULL)
+
+species_over_grid_all <- over_grid_10k_all %>%
+    mutate(id=seq(1:nrow(.))) %>% 
+    left_join(locations_all, by=c("id"="id")) %>% 
+    distinct(CELLCODE,subspeciesname) %>%
+    group_by(CELLCODE) %>%
+    summarize(total_species=n())
+
+
+grid_10km_species_data_all <- grid_10km.wgs_data %>%
+    left_join(species_over_grid_all, by=c("CELLCODE"="CELLCODE"))
+
+### Endemics grid overlap
+over_grid_10k <- sp::over( x = locations_shp , y = grid_10km.wgs , fn = NULL)
+
+species_over_grid <- over_grid_10k %>%
+    mutate(id=seq(1:nrow(.))) %>% 
+    left_join(locations, by=c("id"="id")) %>% 
+    distinct(CELLCODE,subspeciesname) %>%
+    group_by(CELLCODE) %>%
+    summarize(endemic_species=n())
 
 grid_10km_species_data <- grid_10km.wgs_data %>%
-    left_join(species_over_grid, by=c("CELLCODE"="CELLCODE"))
+    left_join(species_over_grid, by=c("CELLCODE"="CELLCODE")) %>%
+    left_join(species_over_grid_all, by=c("CELLCODE"="CELLCODE")) %>% 
+    mutate(hot_spot=if_else(total_species==1,0,endemic_species/total_species))
 
-map_greece_plot_grid_10 <- ggplot()+
+map_greece_plot_grid_endemic <- ggplot()+
   geom_polygon(data = grid_10km_species_data,
-               aes(x=long, y=lat,group = group, fill=n_species),
+               aes(x=long, y=lat,group = group, fill=endemic_species),
                lwd=0.12, color="orange")+
   geom_polygon(data = hellenic_borders_df,
                aes(x=long, y=lat,group = group),
@@ -150,6 +181,57 @@ map_greece_plot_grid_10 <- ggplot()+
                aes(x=long, y=lat,group = group),
                lwd=0.12, fill=NA, color="orange")+
   geom_point(data = locations,aes(x=logD, y=latD,color=Order),size = 0.2)+
+  scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+ 
+  ggtitle("endemic")+
+  labs(x="Longitude",y="Latitude")+
+#  scale_fill_gradientn(colours = c("gray100",terrain.colors(10)),na.value =NA ,name="Number of species")+ #c("gray100","gray50","gray40","gray35","gray30","gray20","gray10","gray0")
+  scale_x_continuous(breaks = seq(23,27,0.5),limits = c(23,27))+
+  scale_y_continuous(breaks = seq(34,36,0.5),limits = c(34,37))+
+  coord_map(xlim = c(23,27), ylim = c(34,37))+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
+#geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
+    
+ggsave("map_greece_plot_grid_endemic.png", plot = map_greece_plot_grid_endemic, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
+
+# total
+map_greece_plot_grid_total <- ggplot()+
+  geom_polygon(data = grid_10km_species_data,
+               aes(x=long, y=lat,group = group, fill=total_species),
+               lwd=0.12, color="orange")+
+  geom_polygon(data = hellenic_borders_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12,color="black")+
+  geom_polygon(data = grid_10km_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12, fill=NA, color="orange")+
+  geom_point(data = locations,aes(x=logD, y=latD,color=Order),size = 0.2)+
+  scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+ 
+  ggtitle("total")+
+  labs(x="Longitude",y="Latitude")+
+#  scale_fill_gradientn(colours = c("gray100",terrain.colors(10)),na.value =NA ,name="Number of species")+ #c("gray100","gray50","gray40","gray35","gray30","gray20","gray10","gray0")
+  scale_x_continuous(breaks = seq(23,27,0.5),limits = c(23,27))+
+  scale_y_continuous(breaks = seq(34,36,0.5),limits = c(34,37))+
+  coord_map(xlim = c(23,27), ylim = c(34,37))+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
+#geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
+    
+ggsave("map_greece_plot_grid_total.png", plot = map_greece_plot_grid_total, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
+### hot spots 
+
+map_greece_plot_grid_hot_spot <- ggplot()+
+  geom_polygon(data = grid_10km_species_data,
+               aes(x=long, y=lat,group = group, fill=hot_spot),
+               lwd=0.12, color="orange")+
+  geom_polygon(data = hellenic_borders_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12,color="black")+
+  geom_polygon(data = grid_10km_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12, fill=NA, color="orange")+
+  geom_point(data = locations,aes(x=logD, y=latD,color=Order),size = 0.2)+
+  ggtitle("hot spot")+
   scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+ 
   labs(x="Longitude",y="Latitude")+
 #  scale_fill_gradientn(colours = c("gray100",terrain.colors(10)),na.value =NA ,name="Number of species")+ #c("gray100","gray50","gray40","gray35","gray30","gray20","gray10","gray0")
@@ -160,13 +242,45 @@ map_greece_plot_grid_10 <- ggplot()+
   theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
 #geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
     
-ggsave("map_greece_plot_grid_10.png", plot = map_greece_plot_grid_10, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
+ggsave("map_greece_plot_grid_hot_spot.png", plot = map_greece_plot_grid_hot_spot, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
 
-grid_10km_species_data_top <- grid_10km_species_data %>% filter(n_species>50)
+# facet hot spot 
+map_greece_plot_grid_hot_spot_facet <- ggplot()+
+  geom_polygon(data = grid_10km_species_data,
+               aes(x=long, y=lat,group = group, fill=hot_spot),
+               lwd=0.12, color="orange")+
+  geom_polygon(data = hellenic_borders_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12,color="black")+
+  geom_polygon(data = grid_10km_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12, fill=NA, color="orange")+
+  geom_point(data = locations,aes(x=logD, y=latD),size = 0.5)+
+  ggtitle("hot spot")+
+  scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+ 
+  labs(x="Longitude",y="Latitude")+
+#  scale_fill_gradientn(colours = c("gray100",terrain.colors(10)),na.value =NA ,name="Number of species")+ #c("gray100","gray50","gray40","gray35","gray30","gray20","gray10","gray0")
+  scale_x_continuous(breaks = seq(23,27,0.5),limits = c(23,27))+
+  scale_y_continuous(breaks = seq(34,36,0.5),limits = c(34,37))+
+  coord_map(xlim = c(23,27), ylim = c(34,37))+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.position = c(0.87, 0.73),
+        legend.text = element_text(size=9),
+        legend.title = element_text(size=10))
+  facet_wrap(~Order)
+    
+ggsave("map_greece_plot_grid_hot_spot.png", plot = map_greece_plot_grid_hot_spot, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
+
+
+
+### only the top species
+grid_10km_species_data_top <- grid_10km_species_data %>% filter(total_species>50)
 
 map_greece_plot_grid_10_top <- ggplot()+
   geom_polygon(data = grid_10km_species_data_top,
-               aes(x=long, y=lat,group = group, fill=n_species),
+               aes(x=long, y=lat,group = group, fill=total_species),
                lwd=0.12, color="orange")+
   geom_polygon(data = hellenic_borders_df,
                aes(x=long, y=lat,group = group),
@@ -188,8 +302,22 @@ map_greece_plot_grid_10_top <- ggplot()+
 ggsave("map_greece_plot_grid_10_top.png", plot =map_greece_plot_grid_10_top, device = "png",width = 30,height = 30,units = "cm",dpi = 300 ,path = "../plots/")
 
 
+
 ## AOO
 ##
 ## see for bootstrap
-AOO <- AOO.computing(locations_shp,Cell_size_AOO =2 )
+AOO_endemic <- AOO.computing(locations_shp,Cell_size_AOO =2 )
+
+AOO_endemic_df <- data.frame(subspeciesname=names(AOO_endemic),AOO=AOO_endemic, row.names=NULL)
+
+endemic_species <- locations %>% 
+    group_by(subspeciesname,Order) %>% 
+    summarize(n_locations=n()) %>% 
+    left_join(AOO_endemic_df, by=c("subspeciesname"="subspeciesname")) %>%
+    left_join(eoo_results, by=c("subspeciesname"="X1")) %>% 
+    mutate(Potentially_EN=if_else(n_locations<5 & (EOO<5000 | AOO<500),TRUE,FALSE)) %>%
+    mutate(Potentially_CR=if_else(n_locations==1 & (EOO<500 | AOO<10),TRUE,FALSE)) %>%
+    mutate(Potentially_VU=if_else(n_locations<10 & (EOO<20000 | AOO<2000),TRUE,FALSE))
+
+write_delim(endemic_species, "endemic_species_iucn.tsv", delim="\t") 
 
