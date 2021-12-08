195c78efb5b74105600a397d0fe8452bbc08e71c
savvas-paragkamian <s.paragkamian@gmail.com>
2021-12-08 21:39:25 +0200

    calculate the maps of vulnerable and CR

--- scripts/arthropoda_crete_assesment.R
+++ scripts/arthropoda_crete_assesment.R
@@ -315,9 +315,212 @@ endemic_species <- locations %>%
     summarize(n_locations=n()) %>% 
     left_join(AOO_endemic_df, by=c("subspeciesname"="subspeciesname")) %>%
     left_join(eoo_results, by=c("subspeciesname"="X1")) %>% 
+    mutate(Potentially_VU=if_else(n_locations<10 & (EOO<20000 | AOO<2000),TRUE,FALSE)) %>%
     mutate(Potentially_EN=if_else(n_locations<5 & (EOO<5000 | AOO<500),TRUE,FALSE)) %>%
     mutate(Potentially_CR=if_else(n_locations==1 & (EOO<500 | AOO<10),TRUE,FALSE)) %>%
-    mutate(Potentially_VU=if_else(n_locations<10 & (EOO<20000 | AOO<2000),TRUE,FALSE))
+    ungroup() %>%
+    mutate(potential_status=if_else(Potentially_CR=="TRUE","Potentially_CR", if_else(Potentially_EN=="TRUE","Potentially_EN",if_else(Potentially_VU=="TRUE","Potentially_VU","FALSE"))))
 
-write_delim(endemic_species, "endemic_species_iucn.tsv", delim="\t") 
+write_delim(endemic_species, "../data/endemic_species_iucn.tsv", delim="\t") 
+
+endemic_species_threatened <- endemic_species %>% filter(potential_status!="FALSE") 
+
+write_delim(endemic_species_threatened, "../data/endemic_species_threatened.tsv", delim="\t") 
+
+endemic_species_cr <- endemic_species %>% filter(potential_status=="Potentially_CR") 
+
+write_delim(endemic_species_cr, "../data/endemic_species_cr.tsv", delim="\t") 
+
+### Threatened maps
+
+locations_threatened <- endemic_species_threatened %>% 
+    dplyr::select(!Order) %>%
+    left_join(arthropods_kriti_endemic,by=c("subspeciesname"="subspeciesname")) %>%
+    filter(latD<38, logD<30) %>%
+    mutate(id=seq(1:nrow(.)))
+
+write_delim(locations_threatened,"../data/locations_threatened.tsv",delim="\t")
+
+locations_threatened_shp <- locations_threatened %>% 
+    dplyr::select(subspeciesname,latD, logD) %>% 
+    relocate(latD,logD,subspeciesname)
+
+coordinates(locations_threatened_shp)<-~ logD+latD
+proj4string(locations_threatened_shp) <- CRS("+proj=longlat +datum=WGS84")
+
+over_grid_10k_thr <- sp::over( x = locations_threatened_shp, y = grid_10km.wgs , fn = NULL)
+
+species_over_grid_thr <- over_grid_10k_thr %>%
+    mutate(id=seq(1:nrow(.))) %>% 
+    left_join(locations_threatened, by=c("id"="id")) %>% 
+    distinct(CELLCODE,subspeciesname) %>%
+    group_by(CELLCODE) %>%
+    summarize(total_species=n())
+
+
+grid_10km_species_data_thr <- grid_10km.wgs_data %>%
+    left_join(species_over_grid_thr, by=c("CELLCODE"="CELLCODE"))
+
+
+map_greece_plot_grid_10_thr <- ggplot()+
+  geom_polygon(data = grid_10km_species_data_thr,
+               aes(x=long, y=lat,group = group, fill=total_species),
+               lwd=0.12, color="orange")+
+  geom_polygon(data = hellenic_borders_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12,color="black")+
+  geom_polygon(data = grid_10km_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12, fill=NA, color="orange")+
+  geom_point(data = locations_threatened,aes(x=logD, y=latD,color=Order),size = 0.2)+
+  scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+ 
+  ggtitle("Potentially Threatened")+
+  labs(x="Longitude",y="Latitude")+
+#  scale_fill_gradientn(colours = c("gray100",terrain.colors(10)),na.value =NA ,name="Number of species")+ #c("gray100","gray50","gray40","gray35","gray30","gray20","gray10","gray0")
+  scale_x_continuous(breaks = seq(23,27,0.5),limits = c(23,27))+
+  scale_y_continuous(breaks = seq(34,36,0.5),limits = c(34,37))+
+  coord_map(xlim = c(23,27), ylim = c(34,37))+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
+#geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
+    
+ggsave("map_greece_plot_grid_10_thr.png", 
+       plot =map_greece_plot_grid_10_thr, 
+       device = "png",
+       width = 30,
+       height = 30,
+       units = "cm",
+       dpi = 300 ,
+       path = "../plots/")
+
+### only the top species
+grid_10km_species_data_trh_5 <- grid_10km_species_data_thr %>% filter(total_species>15)
+
+map_greece_plot_grid_10_thr_5 <- ggplot()+
+  geom_polygon(data = grid_10km_species_data_trh_5,
+               aes(x=long, y=lat,group = group, fill=total_species),
+               lwd=0.12, color="orange")+
+  geom_polygon(data = hellenic_borders_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12,color="black")+
+  geom_polygon(data = grid_10km_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12, fill=NA, color="orange")+
+  geom_point(data = locations_threatened,aes(x=logD, y=latD,color=Order),size = 0.2)+
+  scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+ 
+  labs(x="Longitude",y="Latitude")+
+  ggtitle("Potentially Threatened > 15 " )+
+#  scale_fill_gradientn(colours = c("gray100",terrain.colors(10)),na.value =NA ,name="Number of species")+ #c("gray100","gray50","gray40","gray35","gray30","gray20","gray10","gray0")
+  scale_x_continuous(breaks = seq(23,27,0.5),limits = c(23,27))+
+  scale_y_continuous(breaks = seq(34,36,0.5),limits = c(34,37))+
+  coord_map(xlim = c(23,27), ylim = c(34,37))+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
+#geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
+    
+ggsave("map_greece_plot_grid_10_thr_15.png", 
+       plot =map_greece_plot_grid_10_thr_5, 
+       device = "png",
+       width = 30,
+       height = 30,
+       units = "cm",
+       dpi = 300 ,
+       path = "../plots/")
+
+
+
+### Critically endangered maps
+
+locations_cr <- endemic_species_cr %>% 
+    dplyr::select(!Order) %>%
+    left_join(arthropods_kriti_endemic,by=c("subspeciesname"="subspeciesname")) %>%
+    dplyr::select(subspeciesname,latD, logD, Order) %>% 
+    filter(latD<38, logD<30) %>%
+    mutate("id"=seq(1:nrow(.)))
+
+locations_cr_shp <- locations_cr %>% 
+    relocate(latD,logD,subspeciesname)
+
+coordinates(locations_cr_shp)<-~ logD+latD
+proj4string(locations_cr_shp) <- CRS("+proj=longlat +datum=WGS84")
+
+over_grid_10k_cr <- sp::over( x = locations_cr_shp, y = grid_10km.wgs , fn = NULL)
+
+species_over_grid_cr <- over_grid_10k_cr %>%
+    mutate(id=seq(1:nrow(.))) %>% 
+    left_join(locations_cr, by=c("id"="id")) %>% 
+    distinct(CELLCODE,subspeciesname) %>%
+    group_by(CELLCODE) %>%
+    summarize(total_species=n())
+
+
+grid_10km_species_data_cr <- grid_10km.wgs_data %>%
+    left_join(species_over_grid_cr, by=c("CELLCODE"="CELLCODE"))
+
+
+map_greece_plot_grid_10_cr <- ggplot()+
+  geom_polygon(data = grid_10km_species_data_cr,
+               aes(x=long, y=lat,group = group, fill=total_species),
+               lwd=0.12, color="orange")+
+  geom_polygon(data = hellenic_borders_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12,color="black")+
+  geom_polygon(data = grid_10km_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12, fill=NA, color="orange")+
+  geom_point(data = locations_cr,aes(x=logD, y=latD,color=Order),size = 0.2)+
+  ggtitle("Potentially CR")+
+  scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+ 
+  labs(x="Longitude",y="Latitude")+
+#  scale_fill_gradientn(colours = c("gray100",terrain.colors(10)),na.value =NA ,name="Number of species")+ #c("gray100","gray50","gray40","gray35","gray30","gray20","gray10","gray0")
+  scale_x_continuous(breaks = seq(23,27,0.5),limits = c(23,27))+
+  scale_y_continuous(breaks = seq(34,36,0.5),limits = c(34,37))+
+  coord_map(xlim = c(23,27), ylim = c(34,37))+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
+#geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
+    
+ggsave("map_greece_plot_grid_10_cr.png", 
+       plot =map_greece_plot_grid_10_cr, 
+       device = "png",
+       width = 30,
+       height = 30,
+       units = "cm",
+       dpi = 300 ,
+       path = "../plots/")
+
+
+
+grid_10km_species_data_cr_5 <- grid_10km_species_data_cr %>% filter(total_species>4)
+
+map_greece_plot_grid_10_cr_5 <- ggplot()+
+  geom_polygon(data = grid_10km_species_data_cr_5,
+               aes(x=long, y=lat,group = group, fill=total_species),
+               lwd=0.12, color="orange")+
+  geom_polygon(data = hellenic_borders_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12,color="black")+
+  geom_polygon(data = grid_10km_df,
+               aes(x=long, y=lat,group = group),
+               lwd=0.12, fill=NA, color="orange")+
+  geom_point(data = locations_cr,aes(x=logD, y=latD,color=Order),size = 0.2)+
+  scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+ 
+  labs(x="Longitude",y="Latitude")+
+  ggtitle("Potentially CR > 4 " )+
+#  scale_fill_gradientn(colours = c("gray100",terrain.colors(10)),na.value =NA ,name="Number of species")+ #c("gray100","gray50","gray40","gray35","gray30","gray20","gray10","gray0")
+  scale_x_continuous(breaks = seq(23,27,0.5),limits = c(23,27))+
+  scale_y_continuous(breaks = seq(34,36,0.5),limits = c(34,37))+
+  coord_map(xlim = c(23,27), ylim = c(34,37))+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),legend.position = c(0.87, 0.73),legend.text = element_text(size=9),legend.title = element_text(size=10))
+#geom_text(data = sisquoc, aes(label = paste("  ", as.character(name), sep="")), angle = 60, hjust = 0, color = "yellow")
+    
+ggsave("map_greece_plot_grid_10_cr_5.png", 
+       plot =map_greece_plot_grid_10_cr_5, 
+       device = "png",
+       width = 30,
+       height = 30,
+       units = "cm",
+       dpi = 300 ,
+       path = "../plots/")
 
