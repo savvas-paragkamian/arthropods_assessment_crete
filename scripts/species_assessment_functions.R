a280db0df0b6ab4856da91a060593e2db9b1b1a6
savvas-paragkamian <s.paragkamian@gmail.com>
2022-11-14 12:07:27 +0200

    new script for functions. The first function if for EOO calculation using only the sf package

---
+++ scripts/species_assessment_functions.R
@@ -0,0 +1,32 @@
+#!/usr/bin/Rscript
+
+eoo_calculation <- function(occurrences,land_area) {
+    
+    species <- unique(occurrences$subspeciesname)
+
+    for(s in species){
+
+        n_occurrences <- occurrences %>%
+            filter(subspeciesname==s)
+        
+        rows <- nrow(n_occurrences)
+    
+        if (rows <=3 ){
+
+            print(paste0(s, " has ", rows, 
+                         " occurrences. Moving to the next taxon"))
+            next
+        } else { 
+            print(paste0(s, " has ", rows, " occurrences. Proceeding with 
+                         calculations"))
+            # union the points of each species
+            # and then calculate the convex hull of the points of 
+            # each species
+            species_convex <- st_convex_hull(st_union(n_occurrences))
+            print(class(species_convex))
+            print(st_area(species_convex))
+            species_convex_land <- st_intersection(species_convex,land_area)
+            print(st_area(species_convex_land))
+        }
+    }
+}
