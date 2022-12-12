5df1bcf9a7dfa130486112caa6e1494b0346bc09
savvas-paragkamian <s.paragkamian@gmail.com>
2022-12-12 12:46:26 +0200

    endemic hotstpots and threadspots ready per order

--- scripts/species_assessment.R
+++ scripts/species_assessment.R
@@ -122,12 +122,16 @@ locations_grid <- st_join(crete_grid10, locations_inland, left=TRUE) %>%
 
 ## Here is Crete with all the sampling points
 ##
-g <- ggplot() +
+g_base <- ggplot() +
     geom_sf(crete_shp, mapping=aes()) +
+    geom_sf(natura_crete_land, mapping=aes(),color="orange", alpha=0.2, size=0.1) +
+    coord_sf(crs="WGS84") +
+    theme_bw()
+
+g <- g_base +
     geom_sf(locations_inland, mapping=aes(),color="blue", size=0.1, alpha=0.2) +
     geom_sf(locations_out, mapping=aes(),color="red", size=0.1, alpha=0.2) +
 #    geom_sf(crete_grid10m, mapping=aes(),color="red", alpha=0.2, size=0.1) +
-    geom_sf(natura_crete_land, mapping=aes(fill=SITETYPE, color=SITETYPE), alpha=0.2, size=0.1) +
 #    geom_sf(gr, mapping=aes(),color="orange", alpha=0.2, size=0.1) +
     coord_sf(crs="WGS84") +
     theme_bw()
@@ -191,19 +195,12 @@ endemic_species <- locations_grid %>%
     mutate(potentially_EN=if_else(n_locations<=5 & (eoo<5000 | aoo_area<500),TRUE,FALSE)) %>%
     mutate(potentially_CR=if_else(n_locations==1 & (if_else(is.na(eoo),0,eoo)<100 | aoo_area<10),TRUE,FALSE)) %>%
     ungroup() %>%
-    mutate(paca=if_else(potentially_CR=="TRUE","LT",
-                        if_else(potentially_EN=="TRUE","LT",
-                                if_else(potentially_VU=="TRUE","PT","FALSE"))))
+    mutate(paca=if_else(potentially_CR==TRUE,"LT",
+                        if_else(potentially_EN==TRUE,"LT",
+                                if_else(potentially_VU==TRUE,"PT","FALSE")))) %>%
+    mutate(threatened= if_else(paca=="FALSE",FALSE, TRUE))
 
 write_delim(endemic_species, "../results/endemic_species_paca.tsv", delim="\t") 
-
-endemic_species_threatened <- endemic_species %>% filter(paca!="FALSE") 
-
-write_delim(endemic_species_threatened, "../results/endemic_species_paca_threatened.tsv", delim="\t") 
-
-endemic_species_cr <- endemic_species %>% filter(potentially_CR==TRUE) 
-
-write_delim(endemic_species_cr, "../results/endemic_species_cr.tsv", delim="\t") 
 
 ## Endemic hotspots and Threadspots
 
@@ -215,8 +212,11 @@ endemic_hotspots <- locations_grid %>%
 endemic_hotspots_order <- locations_grid %>% 
     group_by(CELLCODE, Order) %>%
     summarise(n_species=n(), .groups="drop") %>%
-    filter(n_species >= quantile(n_species, 0.90))
+    group_by(Order) %>%
+    mutate(quant90= quantile(n_species, 0.90)) %>%
+    filter(n_species >= quant90)
 
+## Threadspots
 
 threadspots <- locations_grid %>%
     dplyr::select(CELLCODE,subspeciesname) %>%
@@ -224,13 +224,81 @@ threadspots <- locations_grid %>%
     st_drop_geometry() %>% # geometry must be dropped to run pivot wider
     dplyr::group_by(CELLCODE, paca) %>%
     dplyr::summarise(n_species = dplyr::n(), .groups = "drop") %>%
-    tidyr::pivot_wider(names_from = paca, values_from=n_species)
+    tidyr::pivot_wider(names_from = paca, values_from=n_species, values_fill=0)
+
+threadspots_order <- locations_grid %>%
+    dplyr::select(CELLCODE,subspeciesname) %>%
+    left_join(endemic_species, by=c("subspeciesname"="subspeciesname")) %>%
+    st_drop_geometry() %>% # geometry must be dropped to run pivot wider
+    dplyr::group_by(CELLCODE, paca, Order) %>%
+    dplyr::summarise(n_species = dplyr::n(), .groups = "drop") %>%
+    tidyr::pivot_wider(names_from = paca, values_from=n_species, values_fill=0)
+# override the dataframe threadspots with a new one to include the 
+# geometry of the grid.
 
 threadspots <- locations_grid %>% 
     dplyr::select(CELLCODE) %>%
     distinct() %>%
-    inner_join(threadspots, by=c("CELLCODE"="CELLCODE"))
+    inner_join(threadspots, by=c("CELLCODE"="CELLCODE")) %>%
+    mutate(LT.PT_pro=LT/PT) %>%
+    dplyr::select(LT,PT, CELLCODE) %>%
+    mutate(paca_threat=LT+PT) %>%
+    filter(paca_threat>0)
+
+threadspots_lt <- threadspots %>% 
+    filter(paca_threat>= quantile(paca_threat,0.90))
+
+write_delim(threadspots, "../results/threadspots_species_paca.tsv", delim="\t") 
+
+## Per order
+threadspots_order <- locations_grid %>% 
+    dplyr::select(CELLCODE) %>%
+    distinct() %>%
+    inner_join(threadspots_order, by=c("CELLCODE"="CELLCODE")) %>%
+    mutate(LT.PT_pro=LT/PT) %>%
+    dplyr::select(LT,PT, CELLCODE, Order) %>%
+    mutate(paca_threat=LT+PT) %>%
+    filter(paca_threat>0)
+
+threadspots_order_lt <- threadspots_order %>% 
+    group_by(Order) %>%
+    mutate(quant90 = quantile(paca_threat,0.90)) %>%
+    filter(paca_threat>=quant90)
+
+## Crete with endemic hotspots
+##
+
+g_e <- g_base +
+    geom_sf(endemic_hotspots, mapping=aes(fill=n_species), alpha=0.3, size=0.1, na.rm = FALSE) +
+    ggtitle("Endemic hotspots")+
+    scale_fill_gradient(low = "yellow", high = "red", na.value = NA)
+
+ggsave("../plots/crete-hotspots.png", plot=g_e, device="png")
+
+g_e_order <- g_base +
+    geom_sf(endemic_hotspots_order, mapping=aes(fill=n_species), alpha=0.3, size=0.1, na.rm = FALSE) +
+    ggtitle("Endemic hotspots")+
+    scale_fill_gradient(low = "yellow", high = "red", na.value = NA)+
+    facet_grid(rows=vars(Order))
+
+ggsave("../plots/crete-hotspots_order.png", plot=g_e_order, device="png")
+
+## Crete with threadspots
+##
+g_t <- g_base +
+    geom_sf(threadspots_lt, mapping=aes(fill=paca_threat), alpha=0.3, size=0.1, na.rm = TRUE) +
+    ggtitle("Threadspots")+
+    scale_fill_gradient(low = "yellow", high = "red", na.value = "transparent")+
+    theme_bw()
+
+ggsave("../plots/crete-threadspots.png", plot=g_t, device="png")
 
 
+g_t_order <- g_base +
+    geom_sf(threadspots_order_lt, mapping=aes(fill=paca_threat), alpha=0.3, size=0.1, na.rm = TRUE) +
+    ggtitle("Threadspots")+
+    scale_fill_gradient(low = "yellow", high = "red", na.value = "transparent")+
+    facet_grid(rows=vars(Order))
 
+ggsave("../plots/crete-threadspots_order.png", plot=g_t_order, device="png")
 
