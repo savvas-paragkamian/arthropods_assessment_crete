e620a7ae464bbaf0ffbc58381a88be6c8935b02c
savvas-paragkamian <s.paragkamian@gmail.com>
2022-12-19 17:46:33 +0200

    extract dem data for locations

--- scripts/spatial_analysis.R
+++ scripts/spatial_analysis.R
@@ -3,3 +3,44 @@
 library(tidyverse)
 library(sf)
 library(raster)
+source("functions.R")
+
+# Load data
+g_base <- g_base()
+locations_shp <- sf::st_read("../data/arthropods_occurrences/arthropods_occurrences.shp")
+crete_shp <- sf::st_read("../data/crete/crete.shp")
+endemic_species <- read_delim("../results/endemic_species_paca.tsv", delim="\t")
+natura_crete <- sf::st_read("../data/natura2000/natura2000_crete.shp")
+wdpa_crete <- sf::st_read("../data/wdpa_crete/wdpa_crete.shp")
+
+natura_crete_land <- st_intersection(natura_crete, crete_shp)
+
+# split the SPA SCI
+
+natura_crete_land_sci <- natura_crete_land %>% filter(SITETYPE=="B")
+
+## Spatial data
+dem_crete <- raster("../data/dem_crete/dem_crete.tif")
+
+dem_crete_pixel <- as(dem_crete, "SpatialPixelsDataFrame")
+dem_crete_df <- as.data.frame(test_spdf)
+
+
+g_dem <- g_base +
+    geom_raster(dem_crete_df, mapping=aes(x=x, y=y, fill=dem_crete))+
+    geom_sf(locations_shp, mapping=aes(),color="blue", size=0.1, alpha=0.2)
+
+ggsave("../plots/crete_dem.png", plot=g_dem, device="png")
+
+
+locations_shp$elevation <- raster::extract(dem_crete, locations_shp, cellnumbers=F)
+
+
+g_ele <- g_base + 
+    geom_sf(locations_shp, mapping=aes(color=elevation), size=0.1, alpha=0.2) +
+    scale_color_gradientn(colours = terrain.colors(10)) 
+
+ggsave("../plots/crete_occurrences_dem.png", plot=g_ele, device="png")
+
+
+st_write("../results/locations_spatial/locations_spatial.shp")
