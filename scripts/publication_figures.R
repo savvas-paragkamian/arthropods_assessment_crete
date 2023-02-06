70100be66387b7dcc54a3cb0264496e4174b4d0f
savvas-paragkamian <s.paragkamian@gmail.com>
2023-02-06 19:44:46 +0200

    basic crete ready

--- scripts/publication_figures.R
+++ scripts/publication_figures.R
@@ -13,6 +13,10 @@ locations_shp <- sf::st_read("../data/arthropods_occurrences/arthropods_occurren
 locations_spatial <- sf::st_read("../results/locations_spatial/locations_spatial.shp")
 locations_grid <- sf::st_read("../results/locations_grid/locations_grid.shp") 
 crete_shp <- sf::st_read("../data/crete/crete.shp")
+crete_peaks <- read_delim("../data/crete_mountain_peaks.csv", delim=";", col_names=T) %>% 
+    st_as_sf(coords=c("X", "Y"),
+             remove=F,
+             crs="WGS84")
 endemic_species <- read_delim("../results/endemic_species_assessment.tsv", delim="\t")
 clc_crete_shp <- st_read("../data/clc_crete_shp/clc_crete_shp.shp")
 natura_crete <- sf::st_read("../data/natura2000/natura2000_crete.shp")
@@ -37,20 +41,35 @@ crete_base <- ggplot() +
     geom_sf(crete_shp, mapping=aes()) +
     geom_sf(natura_crete_land_sci,
             mapping=aes(fill="Natura2000 SAC"),
-            alpha=0.2,
-            size=0.1,
+#            alpha=0.2,
+#            size=0.1,
+            colour="transparent",
             show.legend=T) +
     geom_sf(locations_inland,
-            mapping=aes(fill = "Occurrences"),
+            mapping=aes(colour="Occurrences"),
             size=0.1,
             alpha=0.5,
             show.legend=T) +
-    scale_fill_manual(values = c("Natura2000 SAC" = "#E69F00","Occurrences" = "#CC79A7"), guide = "legend") +
-    scale_colour_manual(values = c("Occurrences" = "#CC79A7"), guide = "legend") +
-    guides(color=guide_legend(""), fill=guide_legend(""))+
+#    geom_sf(crete_peaks,
+#            mapping=aes(color = "Peaks"),
+#            size=0.5,
+#            alpha=0.5,
+#            show.legend=T) +
+    geom_label(data = crete_peaks, 
+               mapping=aes(x = X, y = Y, label = name),
+               size = 1, nudge_x = 0.05, nudge_y=0.05)+ 
+    scale_fill_manual(values = c("Natura2000 SAC" = "#E69F00"),
+                      guide = "legend") +
+    scale_colour_manual(values = c("Occurrences" = "#CC79A7"),
+                        guide = "legend") +
+    guides(fill = guide_legend(override.aes = list(color = "#E69F00", alpha=1) ),
+           colour = guide_legend(override.aes = list(size = 1,alpha=1, fill="transparent") ) )+
     coord_sf(crs="WGS84") +
     theme_bw()+
-    theme(legend.position = c(0.9,0.8))
+    theme(axis.title=element_blank(),
+          legend.title = element_blank(),
+          legend.position = "bottom",
+          legend.box.background = element_blank())
 
 
 ggsave("../figures/Fig1.tiff", 
