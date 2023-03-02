cda82868168a88041fd60de95f92b5f6d171acb2
savvas-paragkamian <s.paragkamian@gmail.com>
2023-03-02 16:24:16 +0200

    analysis of the IUCN arthropods

--- scripts/get_species_info.R
+++ scripts/get_species_info.R
@@ -1,9 +1,10 @@
 #!/usr/bin/Rscript
 
-## Script name: code_snippets.R
+## Script name: get_species_info.R
 ##
-## Purpose of script: here are some parts of code to crop spatial 
-## objects around the area of Crete and other test code.
+## Purpose of script: use public databases to retrieve information of 
+## species regarding their taxonomy, their global distribution and their 
+## IUCN status
 ##
 ## Author: Savvas Paragkamian
 ##
@@ -23,6 +24,41 @@ locations_spatial <- sf::st_read("../results/locations_spatial/locations_spatial
 locations_grid <- sf::st_read("../results/locations_grid/locations_grid.shp") 
 crete_shp <- sf::st_read("../data/crete/crete.shp")
 endemic_species <- read_delim("../results/endemic_species_assessment.tsv", delim="\t")
+
+
+
+# IUCN dataset from website
+iucn_arthropods <- read_delim("~/Downloads/redlist_species_data_e63da44c-39d3-46bc-819d-1a3203f508bd/points_data.csv",
+                              delim=",") %>% 
+    st_as_sf(coords=c("longitude", "latitude"),
+             remove=FALSE,
+             crs="WGS84")
+iucn_arthropods_species <- iucn_arthropods %>% mutate(subspeciesname=if_else(subspecies=="<NULL>" | is.na(subspecies),
+                                                         sci_name, 
+                                                         paste(sci_name,subspecies))) %>%
+                            distinct(sci_name,subspeciesname)
+
+
+iucn_arthropods_species_u <- unique(iucn_arthropods_species$sci_name)
+
+gbif_iucn <- get_gbifid(iucn_arthropods_species_u,ask=F)
+
+iucn_arthropods_species_gbif <- data.frame(sci_name=iucn_arthropods_species_u, gbifid=gbif_iucn)
+
+classification_iucn <- classification(iucn_arthropods_species_gbif$gbifid.ids, db = 'gbif')
+
+classification_iucn_d <- do.call(rbind, classification_iucn) %>%
+    rownames_to_column(var="gbif") %>% 
+    mutate(gbif = gsub("\\.(.*)","", gbif)) %>%
+    dplyr::select(-id) %>%
+    group_by(gbif,name,rank) %>%
+    dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
+    dplyr::select(-n) %>%
+    pivot_wider(names_from=rank, values_from=name) %>%
+    dplyr::select(-`NA`) %>%
+    filter(!is.na(gbif))
+
+write_delim(classification_iucn_d, "../results/classification_iucn_gbif.tsv", delim="\t")
 
 # Resolve names
 ## gnr_datasources() %>% filter(title=="GBIF Backbone Taxonomy") id=11
@@ -62,7 +98,5 @@ endemic_species_tax <- endemic_species %>%
     left_join(classification_s_d_w, by=c("gbif"="gbif"))
 write_delim(endemic_species_tax, "../results/endemic_species_taxonomy.tsv", delim="\t")
 
-# Red List IUCN
-
-
+endemic_species_tax <- read_delim("../results/endemic_species_taxonomy.tsv", delim="\t")
 
